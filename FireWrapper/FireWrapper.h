76d0d8ca3b44eefe2818eb8ad2ebc8f67c17940f
Semen Kologrivov <semen@sequenia.com>
2018-11-23 09:19:53 +0700

    Initial Commit

---
+++ FireWrapper/FireWrapper.h
@@ -0,0 +1,19 @@
+//
+//  FireWrapper.h
+//  FireWrapper
+//
+//  Created by sequenia on 23/11/2018.
+//  Copyright © 2018 Sequenia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FireWrapper.
+FOUNDATION_EXPORT double FireWrapperVersionNumber;
+
+//! Project version string for FireWrapper.
+FOUNDATION_EXPORT const unsigned char FireWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FireWrapper/PublicHeader.h>
+
+
